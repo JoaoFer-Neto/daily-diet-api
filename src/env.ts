9a6477dd72d693849bad5a759813abd87096eeb4
JoaoFer-Neto <neto.joaofpimenta@gmail.com>
2024-04-23 00:10:38 -0300

    feat(.env): add port variable

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,8 @@ import 'dotenv/config'
 import {z} from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'test', 'production']).default('production')
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  PORT: z.coerce.number().default(3333)
 })
 
 const _env = envSchema.safeParse(process.env)
